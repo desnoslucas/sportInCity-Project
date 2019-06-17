3e801046e91eec10c8f3b9762383258b044a70cb
lucasdesnos <lucasdesnos06@gmail.com>
2019-06-17 15:11:54 +0200

    1er commit

---
+++ src/main/java/com/projetperso/app/SportInCityApplication.java
@@ -0,0 +1,14 @@
+package com.projetperso.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportInCityApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SportInCityApplication.class, args);
+	}
+
+}
